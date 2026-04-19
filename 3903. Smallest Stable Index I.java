aa704a7cc50a150965f6ebf08c3ec2ac97e69af7
Harsh Kumar Verma <harsh9760verma@gmail.com>
2026-04-19 18:11:42 +0530

    Implement firstStableIndex method in Solution class

---
+++ 3903. Smallest Stable Index I.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int firstStableIndex(int[] nums, int k) 
+    {
+        int n=nums.length;
+        int []minr=new int[n];
+        minr[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--)
+            {
+                minr[i]=Math.min(minr[i+1],nums[i]);
+            }
+        int maxl=nums[0];
+        for(int i=0;i<n;i++)
+            {
+                maxl=Math.max(maxl,nums[i]);
+                if(maxl-minr[i]<=k)
+                {
+                    return i;
+                }
+            }
+        return -1;
+    }
+}
