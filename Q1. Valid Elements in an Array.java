d39601e3a5ba0a86712b8240678a37625a0005d6
Harsh Kumar Verma <harsh9760verma@gmail.com>
2026-04-26 09:39:18 +0530

    Add method to find valid elements in an array
    Implement a method to find valid elements in an array based on left and right maximum values.

---
+++ Q1. Valid Elements in an Array.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public List<Integer> findValidElements(int[] nums) 
+    {
+        int n=nums.length;
+        int []lm=new int[n];
+        int []rm=new int[n];
+        lm[0]=Integer.MIN_VALUE;
+        for(int i=1;i<n;i++)
+            {
+                lm[i]=Math.max(lm[i-1],nums[i-1]);
+            }
+        rm[n-1]=Integer.MIN_VALUE;
+        for(int i=n-2;i>=0;i--)
+            {
+                rm[i]=Math.max(rm[i+1],nums[i+1]);
+            }
+        List<Integer> r=new ArrayList<>();
+        for(int i=0;i<n;i++)
+            {
+                if(nums[i]>lm[i]||nums[i]>rm[i])
+                {
+                    r.add(nums[i]);
+                }
+            }
+        return r;
+    }
+}©leetcode
